5c597f4bf5577c5f49f6dda5500c577d1d36078c
Ola Haugen Havrevoll <ola.h.havrevoll@ntnu.no>
2021-08-14 19:13:36 +0200

    Har lagt til debugging og ymse anna

---
+++ aitken/aitken.c
@@ -0,0 +1,31 @@
+#include <assert.h>
+#include "aitken.h"
+
+void aitken(double *seq1, double *seq2, int len)
+{
+    int i;
+    double a, b, c;
+    assert(len > 2);
+    for (i = 0; i < len - 2; i++)
+    {
+        a = seq1[i];
+        b = seq1[i + 1];
+        c = seq1[i + 2];
+        seq2[i] = a - (b - a) * (b - a) / (a - 2 * b + c);
+    }
+}
+
+double aitkenExtrapolate(double *seq1, double *seq2, int len)
+{
+    int n, i, j;
+    n = len / 2;
+    if (len % 2 == 0)
+        n--;
+    for (i = 0; i < n; i++)
+    {
+        aitken(seq1, seq2, len - 2 * i);
+        for (j = 0; j < len - 2 * (i + 1); j++)
+            seq1[j] = seq2[j];
+    }
+    return (len % 2 == 0) ? seq1[1] : seq1[0];
+}

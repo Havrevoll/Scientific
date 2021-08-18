4c1b615f7e3d0a880671c0ac6318559e0ba0b326
Ola Haugen Havrevoll <havrevoll@gmail.com>
2021-08-18 21:58:54 +0200

    Har byrja på factorial-oppgåva i kapittel 2(?)

---
+++ factorial.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void factorial(int *seq, int perms)
+{
+	for (int i = 0; i < n - perms; i++)
+	{
+
+	}
+
+}
+
+void main(int n)
+{
+	int seq[n];
+	for (int i = 0; i < n; i++)
+	{
+		seq[i] = i+1;
+	}
+
+	factorial(seq, n);
+}
+
+
+

35b7076f051436d2dd369aef98ac46a3992ca923
Ola Haugen Havrevoll <havrevoll@gmail.com>
2021-08-19 18:29:06 +0200

    meir oh meir

--- factorial.c
+++ factorial.c
@@ -3,28 +3,32 @@
 void factorial(int *seq, int perms)
 {
 	int j,k,temp
-	for (j = 0; j < n - perms; j++)
-	{
-		factorial(seq, perms-1);
-			temp= seq[k];
-		for (k = 0; k < n - perms; k++)
+		for (j = 0; j < n - perms; j++)
 		{
+			if (perms <n)
+				factorial(seq, perms+1);
+
 			
 
-	}
+			temp= seq[0];
+			for (k = 0; k < n - perms-1; k++)
+				seq[k] = seq[k+1];
 
-}
+			seq[n -perms-2]=temp;
 
-void main(int n)
-{
-	int seq[n];
-	for (int i = 0; i < n; i++)
+
+		}
+
+	void main(int n)
 	{
-		seq[i] = i+1;
-	}
+		int seq[n];
+		for (int i = 0; i < n; i++)
+		{
+			seq[i] = i+1;
+		}
 
-	factorial(seq, 0);
-}
+		factorial(seq, 0);
+	}
 
 
 

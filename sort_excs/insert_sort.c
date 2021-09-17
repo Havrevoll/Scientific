b2a4657ed962443b8ffcbdc3c56a6e2c8cb88335
Ola Haugen Havrevoll <havrevoll@gmail.com>
2021-09-17 09:16:54 +0200

    Byrja på insertsort

---
+++ sort_excs/insert_sort.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+# Dette er insert sort.
+int * insert(int* usortert)
+{
+
+}
+
+int main(int argc, char **argv) 
+{
+	int n = argc;
+
+	int usortert[n-1];
+
+	for (int i = 1; i <n; i++)
+	{
+		usortert[i-1] = atoi(argv[i]);
+
+	}
+
+
+
+}

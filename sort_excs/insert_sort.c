097d13de2711c58e85fa4f1c98877f27a22fab24
Ola Haugen Havrevoll <havrevoll@gmail.com>
2021-09-20 12:17:15 +0200

    byrja på insert sort2

--- sort_excs/insert_sort.c
+++ sort_excs/insert_sort.c
@@ -4,28 +4,38 @@
 # Dette er insert sort.
 int * insert(int* usortert, int len)
 {
-
-	for (int i =1; i<len; i++)
-	{
-		for (int j= i; j >= 0; j--)
-		{
-if (u[j			1;
-		}
-	}
+    int temp;
+    for (int i =1; i<len; i++)
+    {
+        for (int j= i; j > 0; j--)
+        {
+            if (u[j] < u[j-1])
+            {
+                temp=u[j-1];
+                u[j-1] = u[j];
+                u[j]=temp;
+            }
+        }
+    }
 }
 
 int main(int argc, char **argv) 
 {
-	int n = argc;
+    int n = argc;
 
-	int usortert[n-1];
+    int usortert[n-1];
 
-	for (int i = 1; i <n; i++)
-	{
-		usortert[i-1] = atoi(argv[i]);
+    for (int i = 1; i <n; i++)
+    {
+        usortert[i-1] = atoi(argv[i]);
 
-	}
+    }
+    insert(usortert, n-1);
 
-
+    for (int i =0;i<n-1;i++)
+    {
+        printf("%i ",usortert[i]);
+    }
+    printf("\n");
 
 }

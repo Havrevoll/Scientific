b5a0347910759b084b92bebad947758feee450b9
Ola Haugen Havrevoll <havrevoll@gmail.com>
2022-05-18 21:47:42 +0200

    hh

---
+++ c++/Vector.hh
@@ -0,0 +1,19 @@
+#ifndef MYVECTOR
+#define MYVECTOR
+#include <cassert>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Vector{
+private:
+     
+public:
+     ...
+};
+
+#endif

f09bc123781d46766cba0b015f135117db4ef0a0
Ola Haugen Havrevoll <ola.h.havrevoll@ntnu.no>
2022-09-02 18:12:59 +0200

    endra litt på heapsort

--- sort_excs/heap.c
+++ sort_excs/heap.c
@@ -7,16 +7,19 @@ int right(int i);
 
 void max_heapify(int *A, int i, int heapsize);
 void build_max_heap(int *A, int length);
-void heapsort();
+void heapsort(int *A, int length);
 
 void max_heap_insert();
 void heap_extract_max();
 void heap_increase_key();
 void heap_maximum();
 
-void heapsort(int *A)
-{
-}
+/* In order to maintain the max-heap property, we call the procedure MAX-HEAPIFY.
+Its inputs are an array A and an index i into the array. When it is called, MAX-
+HEAPIFY assumes that the binary trees rooted at LEFT(i) and RIGHT(i) are max-
+heaps, but that A[i] might be smaller than its children, thus violating the max-heap
+property. MAX-HEAPIFY lets the value at A[i] "float down" in the max-heap so
+that the subtree rooted at index i obeys the max-heap property.*/
 
 void max_heapify(int *A, int i, int heapsize)
 {
@@ -25,19 +28,13 @@ void max_heapify(int *A, int i, int heapsize)
 
     int largest;
 
-    if (l <= heapsize && A[l] > A[i])
-    {
+    if (l < heapsize && A[l] > A[i])
         largest = l;
-    }
     else
-    {
         largest = i;
-    }
 
-    if (r <= heapsize && A[r] > A[largest])
-    {
+    if (r < heapsize && A[r] > A[largest])
         largest = r;
-    }
 
     if (largest != i)
     {
@@ -53,9 +50,24 @@ void build_max_heap(int *A, int length)
 {
     int heapsize = length;
 
-    for (int i = length/2; i >= 0; i--)
-    {
+    for (int i = (length >> 1) - 1; i >= 0; i--)
         max_heapify(A, i, heapsize);
+}
+
+void heapsort(int *A, int length)
+{
+    int heapsize = length;
+
+    build_max_heap(A, length);
+
+
+    for (int i = length-1; i > 0; i--)
+    {
+        A[i] = A[i] - A[0];
+        A[0] = A[i] + A[0];
+        A[i] = A[0] - A[i];
+        heapsize--;
+        max_heapify(A, 0, heapsize);
     }
 }
 
@@ -65,28 +77,27 @@ int main(int argc, char **argv)
     int usortert[n - 1];
 
     for (int i = 1; i < n; i++)
-    {
         usortert[i - 1] = atoi(argv[i]);
-    }
 
-    // heapsort(usortert, 0, n - 2);
-    build_max_heap(usortert, 10);
+    //max_heapify(usortert, 1, 10);
+    //build_max_heap(usortert, n-1);
+    heapsort(usortert, n - 1);
+    
 
     for (int i = 0; i < n - 1; i++)
-    {
         printf("%i ", usortert[i]);
-    }
+
     printf("\n");
 }
 
 int parent(int i)
 {
-    return (i-1) / 2;
+    return (i - 1) / 2;
 }
 
 int left(int i)
 {
-    return (i << 1)+1;
+    return (i << 1) + 1;
 }
 
 int right(int i)

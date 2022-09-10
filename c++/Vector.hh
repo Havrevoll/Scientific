25939a9c261b8557393efa25c527dae977fe4679
Ola Haugen Havrevoll <ola.h.havrevoll@ntnu.no>
2022-09-10 12:29:30 +0200

    Laga vector

--- c++/Vector.hh
+++ c++/Vector.hh
@@ -11,9 +11,49 @@ using namespace std;
 
 class Vector{
 private:
-     
+     long int size;
+     double *data;
+     int owner;
 public:
-     ...
+     //empty constructor
+     Vector(){
+          size = 0;
+          data = NULL;
+          owner = 0;
+     }
+	
+     //only constructor to allocate space for data
+     Vector(long int  n){
+          size = n;
+          data = (double *)malloc(sizeof(double)*n);
+          owner = 1;
+     }
+  
+     //*this becomes shadow of Vector v 
+     //(copy constructor)
+     Vector(const Vector& v){
+          size = v.size;
+          data = v.data;
+          owner = 0;
+     }  
+     ~Vector(){...}
+     void shadow(double *dataptr, long int len){...}
+     void shadow(const Vector& v){...}
+     void shadow(const Vector& v,  long int i, long int len){...}
+     long int getSize() const{...}
+     double * getRawData() const{...}
+     double& operator()(long int i){...}
+     const double& operator()(long int i) const{.}
+     Vector& operator=(const Vector& v){...}
+     void add(const Vector& v){...}
+     void sub(const Vector& v){...}
+     void mul(const Vector& v){...}
+     void div(const Vector& v){...}
+     void scale(const double x){...}
+     void add_constant(const double x){...}
+     double norm() const{...}
+     void output(const char* fname)const{...}
+     void input(const char* fname){...}
 };
 
 #endif
